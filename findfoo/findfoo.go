4f0be0249975ebed041a92e2a647dfbfdf461775
Reiner Hüchting <dev@reinerhuechting.de>
2022-11-13 14:00:47 +0100

    Zweite Aufgabe hinzugefügt.

--- findfoo/findfoo.go
+++ findfoo/findfoo.go
@@ -10,3 +10,15 @@ func Contains(list []int, x int) bool {
 	}
 	return false
 }
+
+// Erwartet eine Liste von Zahlen und eine Zahl x.
+// Liefert die Position von x in der Liste.
+// Liefert -1, falls x nicht enthalten ist.
+func Find(list []int, x int) int {
+	for pos, el := range list {
+		if el == x {
+			return pos
+		}
+	}
+	return -1
+}
